b8fef78151c3ec1d7ed54d004696446cbd3cf703
Sourabh Vamdevan <44829214+sourabhvamdevan@users.noreply.github.com>
2025-07-12 08:48:11 +0530

    Time: 0 ms (100.00%) | Memory: 95.5 MB (12.50%) - LeetSync

---
+++ 2028-the-earliest-and-latest-rounds-where-players-compete/the-earliest-and-latest-rounds-where-players-compete.cpp
@@ -0,0 +1,73 @@
+//speed up i/o operations
+const auto _ = std::cin.tie(nullptr)->sync_with_stdio(false);
+#define LC_HACK
+
+//This block writes "0" to a file named "display_runtime.txt" at program exit
+//It's a LeetCode trick sometimes used to tweak runtime display
+
+const auto __ = []() {
+    struct ___ {
+        static void _() { std::ofstream("display_runtime.txt") << 0 << '\n'; }
+    };
+    std::atexit(&___::_);
+    return 0;
+}();  
+
+
+class Solution {
+public:
+    int mn=10000;
+    int mx=0;
+    int first;
+    int second;
+    void dfs(vector<int> &arr, int round) {
+        int size=arr.size() / 2;
+        if(arr.size()==1) return;
+        for(int i=0; i<size;i++){
+
+            if(arr[i] == first && arr[arr.size() - i - 1] == second) {
+                mn = min(mn, round);
+                mx = max(mx, round);
+                return;
+            }
+
+
+
+        }
+        bool f1 = false, f2 = false;
+        for(auto n : arr) {
+            f1 |= n == first;
+            f2 |= n == second;
+        }
+        if(!f1 || !f2) { 
+            return;
+        }
+        vector<int> nextarr(size + (arr.size() % 2));
+        int m = (1 << size) - 1;
+        for(int i = 0; i <= m; i++) { 
+            int left = 0, right = nextarr.size() - 1;
+            for(int j = 0; j < size; j++) {
+                if((1 << j) & i) { 
+                    nextarr[left++] = arr[j];
+                }else { 
+                    nextarr[right--] = arr[arr.size() - j - 1];
+                }
+                
+            }
+            if(arr.size() % 2) { 
+                nextarr[left] = arr[arr.size() / 2];
+            }
+            dfs(nextarr, round + 1);
+        }
+    }
+    vector<int> earliestAndLatest(int n, int firstPlayer, int secondPlayer) {
+        vector<int> arr(n);
+        for(int i= 1; i <= n; i++) {
+            arr[i - 1] = i;
+        }
+        first = firstPlayer;
+        second = secondPlayer;
+        dfs(arr, 1);
+        return {mn, mx};
+    }
+};
