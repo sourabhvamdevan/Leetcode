a21eda90415cdf732479e6f8962366f0322e2ce2
Sourabh Vamdevan <44829214+sourabhvamdevan@users.noreply.github.com>
2025-07-15 15:47:53 +0530

    Time: 5 ms (9.58%) | Memory: 59.3 MB (25.79%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -1,12 +1,4 @@
 
-const auto _ = std::cin.tie(nullptr)->sync_with_stdio(false);
-const auto __ = []() {
-    struct ___ {
-        static void _() { std::ofstream("display_runtime.txt") << 0 << '\n'; }
-    };
-    std::atexit(&___::_);
-    return 0;
-}();
 
 
 
