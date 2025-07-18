811e88f249d32151d072d3e5130c57cae0fc5132
Sourabh Vamdevan <44829214+sourabhvamdevan@users.noreply.github.com>
2025-07-18 19:18:13 +0530

    Time: 141 ms (60.82%) | Memory: 154.7 MB (54.12%) - LeetSync

---
+++ 2267-minimum-difference-in-sums-after-removal-of-elements/minimum-difference-in-sums-after-removal-of-elements.cpp
@@ -0,0 +1,61 @@
+class Solution {
+
+    private:
+    #define vll vector<long long>
+    #define ll long long
+public:
+    typedef pair<int, int> P;
+    long long minimumDifference(vector<int>& nums) {
+        int size = nums.size();
+        int n = size / 3;
+
+        // firsty we will do sorting   by sort(nums.begin(),nums.end());  // 1 3 5 7 8 9
+
+        vll minLeftSum(size, 0);
+        vll maxRightSum(size, 0);
+
+       
+        priority_queue<int> maxHeap;
+        ll minSum = 0;
+
+        
+        for (int i = 0; i < 2 * n; i++) {
+            maxHeap.push(nums[i]);
+            minSum += nums[i];
+
+          
+            if (maxHeap.size() > n) {
+                int top = maxHeap.top();
+                minSum = minSum - top;
+                maxHeap.pop();
+            }
+
+          
+            minLeftSum[i] = minSum;
+        }
+
+    
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+        long long maxSum = 0;
+        for (int i = size - 1; i >= n; i--) {
+            minHeap.push(nums[i]);
+            maxSum += nums[i];
+
+            if (minHeap.size() > n) {
+                int top = minHeap.top();
+                maxSum = maxSum - top;
+                minHeap.pop();
+            }
+
+            // otherwise
+            maxRightSum[i] = maxSum;
+        }
+
+      
+        ll res = LLONG_MAX;
+        for (int i = n - 1; i <= 2 * n - 1; i++) {
+            res = min(res, (minLeftSum[i] - maxRightSum[i + 1]));
+        }
+        return res;
+    }
+};
