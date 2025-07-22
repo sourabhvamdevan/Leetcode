d93350dec583c952b42ddd9c05544791136fb95a
Sourabh Vamdevan <44829214+sourabhvamdevan@users.noreply.github.com>
2025-07-22 20:40:23 +0530

    Time: 939 ms (5.50%) | Memory: 11.5 MB (91.84%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,26 +1,10 @@
 class Solution {
 public:
+int get_ans(vector<int>& nums,int& t,int curr,int i){
+if(i>=nums.size()) return curr==t;
+return get_ans(nums,t,curr+nums[i],i+1)+get_ans(nums,t,curr-nums[i],i+1);
+}
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n=nums.size();
-        int range=0;
-        for(int num: nums) range+= num;
-
-        if((target +range) % 2!=0 || target >range ||(target +range)<0) return 0;
-        int sum=(target+range) /2;
-        vector<vector<int>> dp(n+1,vector<int>(sum+1,0));
-        for(int i=0; i<=n; i++) dp[i][0]=1;
-        for(int i=1;i<=n; i++){
-            for(int j=0; j<=sum; j++){
-                if(nums[i-1]<=j){
-                    dp[i][j]=dp[i-1][j-nums[i-1]] +dp[i-1][j];
-
-                }  else {
-                    dp[i][j]=dp[i-1][j];
-                }
-            }
-        }
-
-
-        return dp[n][sum];
+       return get_ans(nums,target,0,0);
     }
 };
