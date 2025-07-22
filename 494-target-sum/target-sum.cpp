7d49c0f9b15ae28554e9bc06d00ec96600443a21
Sourabh Vamdevan <44829214+sourabhvamdevan@users.noreply.github.com>
2025-07-22 21:59:58 +0530

    Time: 942 ms (5.24%) | Memory: 11.7 MB (83.23%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
-int get_ans(vector<int>& nums,int& t,int curr,int i){
-if(i>=nums.size()) return curr==t;
-return get_ans(nums,t,curr+nums[i],i+1)+get_ans(nums,t,curr-nums[i],i+1);
+int get_ans(vector<int>& nums, int &t, int curr, int i){
+    if(i>=nums.size())return curr==t;
+   return get_ans(nums,t,curr+nums[i],i+1)+get_ans(nums,t,curr-nums[i],i+1);
 }
+
     int findTargetSumWays(vector<int>& nums, int target) {
        return get_ans(nums,target,0,0);
     }
