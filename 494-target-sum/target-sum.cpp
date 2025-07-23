0f599b5349e74a6d341ee7c3040d438b048a470c
Sourabh Vamdevan <44829214+sourabhvamdevan@users.noreply.github.com>
2025-07-23 08:56:48 +0530

    Time: 922 ms (7.43%) | Memory: 11.5 MB (91.89%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
-int get_ans(vector<int> &nums,int &t,int curr,int i){
+int sourabh(vector<int> &nums, int &t, int curr,int i){
     if(i>=nums.size())return curr==t;
-    return get_ans(nums,t,curr+nums[i],i+1)+get_ans(nums,t,curr-nums[i],i+1);
+  return sourabh(nums,t,curr+nums[i],i+1)+sourabh(nums,t,curr-nums[i],i+1);  
 }
     int findTargetSumWays(vector<int>& nums, int target) {
-        return get_ans(nums,target,0,0);
+       return sourabh(nums,target,0,0);
+
     
     }
 };
