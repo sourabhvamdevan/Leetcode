924e59bdeda3151286fdb3f3c75d141d538acbe1
Sourabh Vamdevan <44829214+sourabhvamdevan@users.noreply.github.com>
2025-07-29 19:18:22 +0530

    Time: 84 ms (80.49%) | Memory: 13.3 MB (98.23%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,19 +1,27 @@
 class Solution {
+    private:
+    #define vb vector<bool>
 public:
     bool canPartition(vector<int>& nums) {
-        int total=accumulate(nums.begin(),nums.end(),0);
-        if(total %2!=0) return false;
+       int n=nums.size();
+       int sum=0;
+       for(int i=0;i<n;i++)
+        sum+=nums[i];
+        if(sum %2!=0) return false;
+        int target=sum/2;
+vb dp(target+1,false);
+dp[0]=true;
+for(int i=1; i<n;i++){
+    for(int j=target;j>=nums[i]; j--){
+        dp[j]=dp[j] | dp[j-nums[i]];
 
-        int target=total/2;
-        vector<bool> dp(target+1,false);
-        dp[0]=true;
-        for(int num:nums){
-            for(int i=target; i>=num; --i){
-                dp[i]=dp[i] || dp[i-num];
-
-            }
-        }
-
-        return dp[target];
     }
+}
+
+return dp[target];
+
+    }
+
+       
+    
 };
