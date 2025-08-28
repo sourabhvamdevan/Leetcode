0c0fb06810e5bd2204a8e8dd00d9461263b725a3
Sourabh Vamdevan <44829214+sourabhvamdevan@users.noreply.github.com>
2025-08-28 11:48:39 +0530

    Time: 3 ms (98.76%) | Memory: 34.5 MB (95.79%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,18 +1,30 @@
+ #define vvi vector<vector<int>>
+ #define qpii queue<pair<int, int>>
+
+
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int n = mat.size();       
-        int m = mat[0].size();      
-        vector<vector<int>> dis(n, vector<int>(m, -1));  
-        queue<pair<int, int>> q;    
+        int n=mat.size();       
+        int m=mat[0].size();      
+        vvi dis(n, vector<int>(m, -1));  
+        qpii q;    
         
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (mat[i][j] == 0) {
+        for(int i=0;i<n;i++){
+
+
+            for(int j=0;j<m;j++){
+
+                if(mat[i][j]==0)
+                {
                     q.push({i, j});        
                     dis[i][j] = 0;         
                 }
+
+
             }
+
+
         }
 
         int delRow[4] = {1, -1, 0, 0};
@@ -32,6 +44,8 @@ public:
                     q.push({nrow, ncol});                
                 }
             }
+
+            
         }
 
         return dis;
