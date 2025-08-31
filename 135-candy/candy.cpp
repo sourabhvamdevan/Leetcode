28e43e8afde609723b797eed26fdaa44a6794c6b
Sourabh Vamdevan <44829214+sourabhvamdevan@users.noreply.github.com>
2025-08-31 16:55:25 +0530

    Time: 0 ms (100.00%) | Memory: 23 MB (26.03%) - LeetSync

---
+++ 135-candy/candy.cpp
@@ -0,0 +1,40 @@
+#define vi vector<int>
+
+
+class Solution {
+public:
+    int candy(vector<int>& rat) {
+        int n=rat.size(),sum=0;
+        vi res(n,1);
+        if(n==1){
+            return 1;
+
+        }
+
+        for(int i=0;i<n-1;i++)
+        {
+            if(rat[i+1] >rat[i]){
+                res[i+1]=res[i]+1;
+
+            }
+        }
+
+
+        for(int i=n-2; i>=0;i--)
+        {
+if(rat[i]>rat[i+1] && res[i]<=res[i+1])
+{
+    res[i]=res[i+1]+1;
+
+
+}
+
+
+sum+=res[i];
+
+        }
+
+        sum+=res[n-1];
+        return sum;
+    }
+};
