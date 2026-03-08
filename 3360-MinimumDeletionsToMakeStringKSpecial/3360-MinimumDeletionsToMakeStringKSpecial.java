fdfe4ff3d597380b9e85cc98df96224150ada3fa
Sourabh Vamdevan <44829214+sourabhvamdevan@users.noreply.github.com>
2026-03-08 13:20:05 +0530

    Create 3360-MinimumDeletionsToMakeStringKSpecial

---
+++ 3360-MinimumDeletionsToMakeStringKSpecial/3360-MinimumDeletionsToMakeStringKSpecial.java
@@ -0,0 +1,40 @@
+// Last updated: 08/03/2026, 13:24:22
+class Solution {
+    public int minimumDeletions(String word, int k) {
+        int[] freq=new int[26];
+
+        for(char ch : word.toCharArray()){
+            freq[ch-'a']++;
+
+
+        }
+
+        Arrays.sort(freq);
+
+        int result=Integer.MAX_VALUE;
+        int deletedTillNow=0;
+
+        for(int i=0;i<26;i++){
+            int minFreq=freq[i];
+            int temp=deletedTillNow; 
+
+            for(int j=25;j>i;j--){
+                if(freq[j]-freq[i] <= k) 
+                    break;
+
+
+                
+                temp += freq[j] - minFreq - k;
+            }
+
+
+
+            result = Math.min(result, temp);
+            deletedTillNow += minFreq;
+        }
+
+        
+
+        return result;
+    }
+}
