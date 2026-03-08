7a65970c6ee10b5d9f319c4167b36d53204d8d2e
Sourabh Vamdevan <44829214+sourabhvamdevan@users.noreply.github.com>
2026-03-08 13:19:38 +0530

    Create 3601-FindTheKThCharacterInStringGameIi

---
+++ 3601-FindTheKThCharacterInStringGameIi/3601-FindTheKThCharacterInStringGameIi.java
@@ -0,0 +1,16 @@
+// Last updated: 08/03/2026, 13:23:55
+class Solution {
+    public char kthCharacter(long k, int[] arr
+    ) {
+        int cnt=0;
+        for(int i=arr.length; i>=0; i--)
+         if(k>Math.pow(2,i)){
+            k-=Math.pow(2,i);
+            if(arr[i]==1) cnt++;
+
+         }
+
+         cnt %=26;
+         return (char) ('a'+cnt);
+    }
+}
