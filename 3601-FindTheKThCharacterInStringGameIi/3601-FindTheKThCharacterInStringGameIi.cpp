905958aba111c683cd90a8734ce3ac5890b33bd0
Sourabh Vamdevan <44829214+sourabhvamdevan@users.noreply.github.com>
2026-03-08 13:19:34 +0530

    Create 3601-FindTheKThCharacterInStringGameIi

---
+++ 3601-FindTheKThCharacterInStringGameIi/3601-FindTheKThCharacterInStringGameIi.cpp
@@ -0,0 +1,25 @@
+// Last updated: 08/03/2026, 13:23:51
+class Solution {
+public:
+    char kthCharacter(long long k, vector<int>& operations) {
+        string s="";
+        k--;
+        while(k){
+            s+=(k&1) + '0';
+            k>>=1;
+
+        }
+
+        int res=0;
+for(int i=0;i<s.size(); i++){
+    if(s[i]=='1'){
+        res+=operations[i];
+
+    }
+}
+
+
+return 'a' +res %26;
+
+    }
+};
