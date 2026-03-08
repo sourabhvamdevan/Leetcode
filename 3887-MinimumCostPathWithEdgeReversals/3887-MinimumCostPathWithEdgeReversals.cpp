9443a616de271ff0fc1424f09917a0de65ccc4a6
Sourabh Vamdevan <44829214+sourabhvamdevan@users.noreply.github.com>
2026-03-08 13:18:15 +0530

    Create 3887-MinimumCostPathWithEdgeReversals

---
+++ 3887-MinimumCostPathWithEdgeReversals/3887-MinimumCostPathWithEdgeReversals.cpp
@@ -0,0 +1,40 @@
+// Last updated: 08/03/2026, 13:22:32
+class Solution {
+public:
+    int minCost(int n, vector<vector<int>>& edges) {
+        vector<vector<pair<int,int>>> adj(n);
+        vector<int> res(n + 1, INT_MAX);
+
+
+        for (auto &e : edges) {
+            int u = e[0];
+            int v = e[1];
+            int w = e[2];
+
+            adj[u].push_back({v, w});      
+            adj[v].push_back({u, w * 2});   
+        }
+
+       
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        pq.push({0, 0});
+        res[0] = 0;
+
+        while (!pq.empty()) {
+            auto [dist, currNode] = pq.top();
+            pq.pop();
+
+            for (auto &edge : adj[currNode]) {
+                int nextNode = edge.first;
+                int cost = edge.second;
+
+                if (dist + cost < res[nextNode]) {
+                    res[nextNode] = dist + cost;
+                    pq.push({res[nextNode], nextNode});
+                }
+            }
+        }
+
+        return res[n - 1] == INT_MAX ? -1 : res[n - 1];
+    }
+};
