71bfcd1cfb2403f331484ab09d882b456815b6e1
Sourabh Vamdevan <44829214+sourabhvamdevan@users.noreply.github.com>
2026-03-08 13:19:52 +0530

    Create 3475-MinimumOperationsToMakeBinaryArrayElementsEqualToOneI

---
+++ 3475-MinimumOperationsToMakeBinaryArrayElementsEqualToOneI/3475-MinimumOperationsToMakeBinaryArrayElementsEqualToOneI.java
@@ -0,0 +1,25 @@
+// Last updated: 08/03/2026, 13:24:09
+class Solution{
+    public int minOperations(int[] nums){
+        int n = nums.length;
+        int operations = 0;
+        
+        
+        for (int i = 0; i <= n - 3; i++){
+            if (nums[i] ==0) {
+                
+                nums[i] ^= 1;
+                nums[i+1] ^= 1;
+                nums[i+2] ^= 1;
+                operations++;
+            }
+        }
+        
+        
+        if(nums[n-2] == 0 || nums[n-1] == 0){
+            return -1;
+        }
+        
+        return operations;
+    }
+}
