274e1557cfffef7c3327f556901ea03b1748e809
Sourabh Vamdevan <44829214+sourabhvamdevan@users.noreply.github.com>
2026-03-27 21:28:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Floyd Warshall/floyd-warshall.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void floydWarshall(int[][] dist) {
+        int n = dist.length;
+
+   
+        for (int k = 0; k < n; k++) {
+           
+            for (int i = 0; i < n; i++) {
+               
+                for (int j = 0; j < n; j++) {
+                    
+                 
+                    if (dist[i][k] != 1e8 && dist[k][j] != 1e8){
+                        if (dist[i][k] + dist[k][j] < dist[i][j]) {
+                            dist[i][j] = dist[i][k] + dist[k][j];
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
