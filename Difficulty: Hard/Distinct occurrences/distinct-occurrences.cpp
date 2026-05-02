5c878faec402993a04dd8511847e901ed2a317f6
Sourabh Vamdevan <44829214+sourabhvamdevan@users.noreply.github.com>
2026-05-02 14:30:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Distinct occurrences/distinct-occurrences.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int subseqCount(string &txt, string &pat) {
+        int n=txt.size();
+        int m=pat.size();
+        
+    
+        if (m>n) return 0;
+        
+       
+        vector<vector<unsigned int>> dp(m + 1, vector<unsigned int>(n + 1, 0));
+        
+   
+        for(int j=0;j<=n;j++){
+            dp[0][j]=1;
+        }
+        
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+               
+                dp[i][j]=dp[i][j - 1];
+                
+             
+        if (pat[i - 1]==txt[j - 1])
+        {
+                    dp[i][j] = (dp[i][j] + dp[i - 1][j - 1]);
+     }
+            }
+            
+            
+            
+        }
+        
+        return dp[m][n];
+    }
+};
