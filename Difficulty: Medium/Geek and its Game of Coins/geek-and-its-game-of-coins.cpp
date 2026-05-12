b178b19e0d715c6953fc2eca162a193710c35c7c
Sourabh Vamdevan <44829214+sourabhvamdevan@users.noreply.github.com>
2026-05-12 17:05:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Geek and its Game of Coins/geek-and-its-game-of-coins.cpp
@@ -0,0 +1,39 @@
+class Solution {
+  public:
+    int findWinner(int n, int x, int y) {
+     
+        vector<bool> dp(n + 1, false);
+
+    
+        dp[0] = false;
+        
+
+        for (int i = 1; i <= n; i++)
+        
+        {
+            
+      
+            if (i - 1 >= 0 && !dp[i - 1])
+            
+            {
+                dp[i] = true;
+            }
+ 
+            else if (i - x >= 0 && !dp[i - x])
+            
+            {
+                dp[i] = true;
+            }
+        
+            else if (i - y >= 0 && !dp[i - y]) {
+                dp[i] = true;
+            }
+         
+            else {
+                dp[i] = false;
+            }
+        }
+
+        return dp[n] ? 1 : 0;
+    }
+};
